312f2fc83b762a394f464dcf31fd11403251727f
Kerlegand, Andrew Gerardo <andrew.kerlegand@ge.com>
2019-11-04 14:34:14 -0600

    test when there are two past questions

--- polls/tests.py
+++ polls/tests.py
@@ -90,3 +90,15 @@ class QuestionIndexViewTests(TestCase):
             response.context['latest_question_list'],
             ['<Question: Past question.>']
         )
+
+    def test_two_past_questions(self):
+        """
+        The questions index page may display multiple questions.
+        """
+        create_question(question_text="Past question 1.", days=-30)
+        create_question(question_text="Past question 2.", days=-5)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            ['<Question: Past question 2.>', '<Question: Past question 1.>']
+        )
