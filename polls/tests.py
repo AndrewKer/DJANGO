34076c9b35d6fe0b0d388dcad845a89c3ff95dd0
Kerlegand, Andrew Gerardo <andrew.kerlegand@ge.com>
2019-11-04 15:21:10 -0600

    test past question can be viewed correctly

--- polls/tests.py
+++ polls/tests.py
@@ -113,3 +113,13 @@ class QuestionDetailViewTests(TestCase):
         url = reverse('polls:detail', args=(future_question.id,))
         response = self.client.get(url)
         self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        """
+        The detail view of a question with a pub_date in the past
+        displays the question's text.
+        """
+        past_question = create_question(question_text='Past Question.', days=-5)
+        url = reverse('polls:detail', args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
