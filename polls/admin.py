9b542d55a11a6c49a57aad31ff0ee642aca9565a
Kerlegand, Andrew Gerardo <andrew.kerlegand@ge.com>
2019-11-05 09:38:42 -0600

    split the form up into fieldsets

--- polls/admin.py
+++ polls/admin.py
@@ -3,7 +3,10 @@ from django.contrib import admin
 from .models import Question, Choice
 
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ['pub_date', 'question_text']
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
 
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
