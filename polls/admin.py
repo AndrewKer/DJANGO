cbdbea1f4ef94cdf4fa7b73d9097b35239246f8d
Kerlegand, Andrew Gerardo <andrew.kerlegand@ge.com>
2019-11-05 09:37:27 -0600

    customizing admin form
    ou’ll follow this pattern – create a model admin class, then pass it as the second argument to admin.site.register() – any time you need to change the admin options for a model.
    
    This particular change above makes the “Publication date” come before the “Question” field

--- polls/admin.py
+++ polls/admin.py
@@ -2,5 +2,8 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
